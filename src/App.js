2f96b68853977ee067cf74c5f7077e5b8634c636
Rajat Gupta <rajat295gupta@gmail.com>
2020-01-19 16:41:23 +0530

    components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./Components/navbar.component";
+import ExerciseList from "./Components/exerciseList.component";
+import EditExercise from "./Components/editExercise.component";
+import createExercise from "./Components/createExercise.component";
+import createUser from "./Components/createUser.component";
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <div className = "container">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={ ExerciseList } />
+        <Route path="/create" component={ createExercise } />
+        <Route path="/user" component={ createUser } />
+      </div>
     </Router>
   );
 }
---
+++ src/Components/createUser.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class createUser extends Component {
+	render() {
+		return (
+			<div>
+				<p>You are on Create User Page</p>
+			</div>
+		);
+	}
+}
