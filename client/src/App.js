3427fc54b9b3d8eb82b3b923a792d65ab3c74328
Rajat Gupta <rajat295gupta@gmail.com>
2020-04-29 21:41:48 +0530

    small change

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,37 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 import Navbar from "./Components/navbar.component";
 import ExerciseList from "./Components/exerciseList.component";
 import EditExercise from "./Components/editExercise.component";
 import createExercise from "./Components/createExercise.component";
-import createUser from "./Components/createUser.component";
+import CreateUser from "./Components/createUser.component"; 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogined : true,
+    }
+  }
+
   render() {
+  const {isLogined} = this.state;
+
   return (
+    isLogined ? <div>
     <Router>
       <div className="container">
         <Navbar />
         <br />
         <Route path="/" exact component={ ExerciseList } />
-        <Route path='/edit/:id' component={EditExercise} />
+        <Route path='/edit/:id' component={ EditExercise } />
         <Route path="/create" component={ createExercise } />
-        <Route path="/user" component={ createUser } />
       </div>
     </Router>
+    </div>
+    : <CreateUser />
   );
 }
 }
